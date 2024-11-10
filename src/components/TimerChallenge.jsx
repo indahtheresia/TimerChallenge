5185e0c10c11dba258b9744b727a062812f11ad7
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-11-10 20:58:51 +0700

    forward ref to custom components

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,12 +2,14 @@ import { useState, useRef } from 'react';
 import ResultModal from './ResultModal.jsx';
 export default function TimerChallenge({title, targetTime}) {
   const timer = useRef();
+  const dialog = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -18,7 +20,7 @@ export default function TimerChallenge({title, targetTime}) {
 
   return (
     <>
-      {timerExpired && <ResultModal result= "lost" targetTime={targetTime} />}
+      <ResultModal ref={dialog} result= "lost" targetTime={targetTime} />
       <section className="challenge">
         <h2>{title}</h2>
         {timerExpired && <p>You lost!</p>}
