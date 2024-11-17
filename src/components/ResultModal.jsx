964d204389c77f7d6cccea9f2be3c6e4cfc5a186
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-11-17 10:30:10 +0800

    create portal (move dialog to modal div)

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({timeRemaining, targetTime, onReset}, ref) {
   const dialog = useRef();
@@ -12,7 +13,7 @@ const ResultModal = forwardRef(function ResultModal({timeRemaining, targetTime, 
       }
     };
   });
-  return <dialog ref={dialog} className="result-modal" onClose={onReset}>
+  return createPortal(<dialog ref={dialog} className="result-modal" onClose={onReset}>
     {userLost && <h2>You lost</h2>}
     {!userLost && <h2>Your score: {score}</h2>}
     <p>The target time was <strong>{targetTime} seconds.</strong></p>
@@ -20,7 +21,8 @@ const ResultModal = forwardRef(function ResultModal({timeRemaining, targetTime, 
     <form method="dialog" onSubmit={onReset}>
       <button>Close</button>
     </form>
-  </dialog>;
+  </dialog>, 
+  document.getElementById('modal'));
 })
 
 export default ResultModal;
